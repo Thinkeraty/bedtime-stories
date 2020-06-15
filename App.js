2a542adb9fc5faa9d45654f789eb4d352b9fd919
Samyak Jain <contact.thinkeraty@gmail.com>
2020-06-15 19:02:02 +0530

    Bottom Tab Navigation Images

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 import ReadStory from './screens/ReadSory';
 import WriteStory from './screens/WriteStory';
@@ -18,7 +18,32 @@ export default class App extends React.Component {
 const TabNavigator = createBottomTabNavigator({
   Read: {screen: ReadStory},
   Write: {screen: WriteStory}
-})
+},
+{
+    defaultNavigationOptions: ({navigation}) => 
+      ({
+        tabBarIcon: () => {
+          const routeName = navigation.state.routeName;
+
+          if(routeName === 'Read') {
+            return (
+              <Image 
+                source={require('./assets/booklogo.jpg')}
+                style={{width: 40, height: 40}}
+              />
+            )
+          } else if(routeName === 'Write') {
+            return(
+              <Image 
+                source={require('./assets/writing.png')}
+                style={{width: 40, height : 40}}
+              />
+            )
+          }
+        }
+      })
+  }
+)
 
 const AppContainer = createAppContainer(TabNavigator)
 
