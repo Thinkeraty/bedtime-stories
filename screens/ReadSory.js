dc40a9c25adb6444654b3e09a16096e42e59a6f8
Thinkeraty <contact.thinkeraty@gmail.com>
2020-07-01 10:39:54 +0530

    Search and FlatList

--- screens/ReadSory.js
+++ screens/ReadSory.js
@@ -1,12 +1,112 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, FlatList, ToastAndroid, ScrollView } from 'react-native';
+import {Header, SearchBar} from 'react-native-elements';
+import db from '../config.js'
 
-export default class ReadStory extends React.Component {
-    render() {
+export default class ReadStoryScreen extends React.Component {
+    constructor(){
+      super();
+      this.state ={
+        allStories:[],
+        dataSource:[],
+        search : ''
+      }
+    }
+    componentDidMount(){
+      this.retrieveStories()
+    }
+  
+    updateSearch = search => {
+      this.setState({ search });
+    };
+  
+  
+    retrieveStories=()=>{
+      try {
+        var allStories= []
+        var stories = db.collection("Stories")
+          .get().then((querySnapshot)=> {
+            querySnapshot.forEach((doc)=> {
+                // doc.data() is never undefined for query doc snapshots
+                
+                allStories.push(doc.data())
+                console.log('this are the stories',allStories)
+            })
+            this.setState({allStories})
+          })
+      }
+      catch (error) {
+        console.log(error);
+      }
+    };
+  
+  
+    SearchFilterFunction(text) {
+      //passing the inserted text in textinput
+      const newData = this.state.allStories.filter((item)=> {
+        //applying filter for the inserted text in search bar
+        const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      this.setState({
+        //setting the filtered newData on datasource
+        //After setting the data it will automatically re-render the view
+        dataSource: newData,
+        search: text,
+      });
+    }
+  
+      render(){
         return(
-            <View style={{flex: 1, alignItems: 'center', textAlign: 'center', marginTop: 100, marginBottom: 100}}>
-                <Text style={{fontWeight: 'bold', fontSize: 20}}>There was once a king and a queen.... You decide the rest.</Text>
-            </View>
-        )
+          <View style ={styles.container}>
+                <View styles ={{height:20,width:'100%'}}>
+              <SearchBar
+              placeholder="Type Here..."
+              onChangeText={text => this.SearchFilterFunction(text)}
+              onClear={text => this.SearchFilterFunction('')}
+              value={this.state.search}
+            />
+          </View>
+          
+          <FlatList
+                data={this.state.search === "" ?  this.state.allStories: this.state.dataSource}
+                renderItem={({ item }) => (
+                  <View style={styles.itemContainer}>
+                    <Text>  Title: {item.storyName}</Text>
+                    <Text>  Author : {item.name}</Text>
+                  </View>
+                )}
+                keyExtractor={(item, index) => index.toString()}
+                /> 
+          
+          
+          
+        </View>  
+      );      
     }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF'
+  },
+  item: {
+    backgroundColor: 'purple',
+    padding:10,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+  itemContainer: {
+    height: 80,
+    width:'100%',
+    borderWidth: 2,
+    borderColor: 'purple',
+    justifyContent:'center',
+    alignSelf: 'center',
+  }
+});
